0fe43f87abc16b797dfacd2574ed9c8c5de2eab1
Jesse Brown <brownjesse8895@yahoo.com>
2021-01-29 20:47:32 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -62,4 +62,5 @@ def add_characters
   epic_tragedy[:additional_characters] = []
   epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
 epic_tragedy[:additional_characters] << { name: "Apothecary" }
+epic_tragedy
 end
